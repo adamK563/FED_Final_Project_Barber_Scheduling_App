0cd8e2d47219afcc9b796c01bc206b089084ab3b
Adam Karpovich <83719998+adamK563@users.noreply.github.com>
2023-09-01 19:25:51 +0300

    Update idb.js

--- FED2023-Adam-Veronika/src/utils/idb.js
+++ FED2023-Adam-Veronika/src/utils/idb.js
@@ -1,29 +1,37 @@
+// Define the database and store names
 const DB_NAME = "barber_appointments";
 const STORE_NAME = "appointments";
 
+// Function to open the IndexedDB database
 const openDB = () => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(DB_NAME, 1);
 
+    // Event handler for database upgrade (schema changes)
     request.onupgradeneeded = (event) => {
       const db = event.target.result;
       const store = db.createObjectStore(STORE_NAME, {
         keyPath: "id",
         autoIncrement: true
       });
+
+      // Create an index for searching by date
       store.createIndex("date", "date", { unique: false });
     };
 
+    // Event handler for successful database opening
     request.onsuccess = (event) => {
       resolve(event.target.result);
     };
 
+    // Event handler for errors during database opening
     request.onerror = (event) => {
       reject(event.target.error);
     };
   });
 };
 
+// Function to add an appointment to the IndexedDB
 const addAppointment = async (appointment) => {
   const db = await openDB();
   const transaction = db.transaction(STORE_NAME, "readwrite");
@@ -40,6 +48,7 @@ const addAppointment = async (appointment) => {
   });
 };
 
+// Function to retrieve appointments from the IndexedDB
 const getAppointments = async () => {
   const db = await openDB();
   const transaction = db.transaction(STORE_NAME, "readonly");

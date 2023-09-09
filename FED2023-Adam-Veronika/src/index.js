e9b11a36ee3fbec8e0efa64d72b1665ac3a1d318
Adam Karpovich <83719998+adamK563@users.noreply.github.com>
2023-09-09 12:58:17 +0300

    Update index.js

--- FED2023-Adam-Veronika/src/index.js
+++ FED2023-Adam-Veronika/src/index.js
@@ -1,3 +1,7 @@
+/*
+  Adam Karpovich 314080383
+  Veronika Kovaleva 321777583
+*/
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App"; // Replace with the correct path to your App component
